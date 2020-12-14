bef142b4338d5c4be9af1d377ee3d7ba83a2d07a
Isaac Ashwin <ashiswin@gmail.com>
2020-12-15 06:51:03 +0800

    Added checks for less than minimum dawning essence

--- app.py
+++ app.py
@@ -4,45 +4,46 @@ from flask_cors import CORS
 import numpy as np
 import cvxpy as cp
 
-app = Flask(__name__, static_url_path='')
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
+MASTERWORKED_ESSENCE = 10
+NOMASTERWORK_ESSENCE = 15
+
+app = Flask(__name__, static_url_path="")
+cors = CORS(
+    app,
+    resources={r"/calculate": {"origins": "https://dawning-optimizer.netlify.app/"}},
+)
+app.config["CORS_HEADERS"] = "Content-Type"
 
 ingredients = []
 
-with open('ingredients.csv') as f:
+with open("ingredients.csv") as f:
     for i in f:
         ingredients.append(i.strip())
 
 recipes = []
 cost = None
 
-with open('cost.csv') as f:
-    recipes = f.readline().split(',')
+with open("cost.csv") as f:
+    recipes = f.readline().split(",")
 
     cost = np.zeros((len(ingredients), len(recipes)))
 
     i = 0
     for line in f:
-        row = line.split(',')
+        row = line.split(",")
 
         for j in range(len(row)):
             cost[i][j] = int(row[j])
-        
-        i += 1
-        
-@app.route('/calculate', methods=['POST'])
-def calculate():
-    amounts = []
-    for ingredient in ingredients:
-        if ingredient in request.json and request.json[ingredient] != "":
-            amounts.append(request.json[ingredient])
-        else:
-            amounts.append(0)
-    amounts = np.array(amounts, dtype=np.int64)
 
-    # Calculate without masterwork
-    cost[-1] = np.ones(len(recipes)) * 15
+        i += 1
+
+
+def solveILP(amounts, essence_cost):
+    # Essence of Dawning guaranteed to be the last item in the list
+    if amounts[-1] < essence_cost:
+        return {"total": 0, "items": []}
+
+    cost[-1] = np.ones(len(recipes)) * essence_cost
     x = cp.Variable(len(recipes), integer=True)
 
     objective = cp.Maximize(cp.sum(cost * x))
@@ -57,22 +58,29 @@ def calculate():
         if x.value[i] > 0:
             result.append((recipes[i], x.value[i]))
     result.sort(key=lambda x: -x[1])
-    response = {'total': np.sum(x.value), 'items': result}
+    return {"total": np.sum(x.value), "items": result}
+
+
+@app.route("/calculate", methods=["POST"])
+def calculate():
+    amounts = []
+    for ingredient in ingredients:
+        if ingredient in request.json and request.json[ingredient] != "":
+            amounts.append(request.json[ingredient])
+        else:
+            amounts.append(0)
+    amounts = np.array(amounts, dtype=np.int64)
+
+    # Calculate without masterwork
+    response = solveILP(amounts, NOMASTERWORK_ESSENCE)
 
     # Calculate with masterwork
-    cost[-1] = np.ones(len(recipes)) * 10
-
-    prob.solve(solver=cp.GLPK_MI)
-    mwresult = []
-
-    for i in range(x.value.shape[0]):
-        if x.value[i] > 0:
-            mwresult.append((recipes[i], x.value[i]))
-    mwresult.sort(key=lambda x: -x[1])
-    response['mwtotal'] = np.sum(x.value)
-    response['mwitems'] = mwresult
+    mwresult = solveILP(amounts, MASTERWORKED_ESSENCE)
+    response["mwtotal"] = mwresult["total"]
+    response["mwitems"] = mwresult["items"]
 
     return jsonify(response)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
